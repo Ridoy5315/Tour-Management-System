eb67c93d927342df0d1cadbdd93b428775c69e52
Ridoy5315 <ridoy.st99@gmail.com>
2025-07-19 16:39:46 +0800

    code of tour and development has been completed

---
+++ src/app/modules/division/division.service.ts
@@ -0,0 +1,75 @@
+import { IDivision } from "./division.interface";
+import httpStatus from "http-status-codes";
+import AppError from "../../errorHelpers/AppError";
+import { Division } from "./division.model";
+
+const createDivision = async (payload: IDivision) => {
+  const existingDivision = await Division.findOne({ name: payload.name });
+  if (existingDivision) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User ALready Exist");
+  }
+
+  const division = await Division.create(payload);
+
+  return division;
+};
+
+const getAllDivision = async () => {
+  const divisions = await Division.find({});
+
+  const totalDivisions = await Division.countDocuments();
+
+  return {
+    data: divisions,
+    meta: {
+      total: totalDivisions,
+    },
+  };
+};
+
+const getSingleDivision = async (slug: string) => {
+  const division = await Division.findOne({ slug });
+
+  return {
+    data: division,
+  };
+};
+
+const updateDivision = async (id: string, payload: Partial<IDivision>) => {
+  const existingDivision = await Division.findById(id);
+  if (!existingDivision) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Division not found.");
+  }
+
+  const duplicateDivision = await Division.findOne({
+    name: payload.name,
+    _id: { $ne: id },
+  });
+
+  if (duplicateDivision) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "A division with this name already exists."
+    );
+  }
+
+  const updatedDivision = await Division.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  return updatedDivision;
+};
+
+const deleteDivision = async (id: string) => {
+  await Division.findByIdAndDelete(id);
+  return null;
+};
+
+export const DivisionServices = {
+  createDivision,
+  getAllDivision,
+  getSingleDivision,
+  updateDivision,
+  deleteDivision,
+};
---
+++ src/app/modules/tour/tour.service.ts
@@ -0,0 +1,122 @@
+import { Tour, TourType } from "./tour.model";
+import { ITour, ITourType } from "./tour.interface";
+import AppError from "../../errorHelpers/AppError";
+import httpStatus from "http-status-codes";
+import { tourSearchableFields } from "./tour.constant";
+import { excludeField } from "../../constant";
+
+const createTour = async (payload: ITour) => {
+  const existingTour = await Tour.findOne({ title: payload.title });
+  if (existingTour) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "A tour with this title already exists."
+    );
+  }
+
+  const tour = await Tour.create(payload);
+
+  return tour;
+};
+
+const getAllTours = async (query : Record<string, string>) => {
+
+  const filter = query;
+  const searchTerm = query.searchTerm || "";
+  const sort = query.sort || "-createdAt";
+  const fields = query.fields?.split(",").join(" ") || "";
+  const page = Number(query.page) || 1;
+  const limit = Number(query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  for(const field of excludeField){
+    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+    delete filter[field]
+  }
+
+  const searchQuery = { $or: tourSearchableFields.map(field => ( {[field]: {$regex : searchTerm, $options: "i"}}))}
+
+
+  const filterQuery = Tour.find(filter);
+  const tours = filterQuery.find(searchQuery);
+  const allTours = await tours.sort(sort).select(fields).skip(skip).limit(limit);
+
+  const totalTours = await Tour.countDocuments();
+
+  const totalPages = Math.ceil(totalTours/limit)
+
+  const meta = {
+    page: page,
+    limit : limit,
+    total: totalTours,
+    totalPage: totalPages
+  }
+  
+  return {
+    data: allTours,
+    meta: meta
+  }
+};
+
+const updateTour = async (id: string, payload: Partial<ITour>) => {
+  const existingTour = await Tour.findById(id);
+
+  if (!existingTour) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Tour Not Found");
+  }
+
+  const updatedTour = await Tour.findByIdAndUpdate(id, payload, { new: true });
+
+  return updatedTour;
+};
+
+const deleteTour = async (id: string) => {
+  return await Tour.findByIdAndDelete(id);
+};
+
+const createTourType = async (payload: ITourType) => {
+  const existingTourType = await TourType.findOne({ name: payload.name });
+
+  if (existingTourType) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Tour type already exists.");
+  }
+
+  return await TourType.create({ name });
+};
+
+const getAllTourTypes = async () => {
+  return await TourType.find();
+};
+
+const updateTourType = async (id: string, payload: ITourType) => {
+  const existingTourType = await TourType.findById(id);
+
+  if (!existingTourType) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Tour type not found.");
+  }
+
+  const updatedTourType = await TourType.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return updatedTourType;
+};
+
+const deleteTourType = async (id: string) => {
+  const existingTourType = await TourType.findById(id);
+  if (!existingTourType) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Tour type not found.");
+  }
+
+  return await TourType.findByIdAndDelete(id);
+};
+
+export const TourService = {
+  createTour,
+  getAllTours,
+  updateTour,
+  deleteTour,
+  createTourType,
+  getAllTourTypes,
+  updateTourType,
+  deleteTourType,
+};
