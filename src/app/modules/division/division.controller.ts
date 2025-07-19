295db11805b06a0977d327b0f872ff039b3822bc
Ridoy5315 <ridoy.st99@gmail.com>
2025-07-19 16:39:46 +0800

    code of tour and development has been completed

---
+++ src/app/modules/division/division.controller.ts
@@ -0,0 +1,73 @@
+import { sendResponse } from './../../utils/sendResponse';
+import { Request, Response, } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { DivisionServices } from "./division.service";
+
+
+const createDivision = catchAsync(async(req: Request, res: Response) => {
+     const result = await DivisionServices.createDivision(req.body);
+     sendResponse(res,  {
+          statusCode: 201,
+          success: true,
+          message: "Division created",
+          data: result
+     })
+});
+
+const getAllDivision = catchAsync(async(req: Request, res: Response) =>{
+     const result = await DivisionServices.getAllDivision();
+     sendResponse(res,  {
+          statusCode: 201,
+          success: true,
+          message: "Divisions retrieved",
+          data: result.data,
+          meta: result.meta
+     })
+})
+
+const getSingleDivision = catchAsync(async(req: Request, res: Response) =>{
+     const slug = req.params.slug;
+     const result = await DivisionServices.getSingleDivision(slug);
+     sendResponse(res,  {
+          statusCode: 200,
+          success: true,
+          message: "Division retrieved",
+          data: result.data
+     })
+})
+
+
+
+const updateDivision = catchAsync(async(req: Request, res: Response) => {
+     const id = req.params.id;
+     const result = await DivisionServices.updateDivision(id, req.body);
+
+     sendResponse(res,  {
+          statusCode: 200,
+          success: true,
+          message: "Division updated",
+          data: result
+     })
+
+})
+
+const deleteDivision = catchAsync(async(req: Request, res: Response) => {
+     const result = await DivisionServices.deleteDivision(req.params.id);
+
+     sendResponse(res,  {
+          statusCode: 200,
+          success: true,
+          message: "Division deleted",
+          data: result
+     })
+
+})
+
+export const DivisionController = {
+     createDivision,
+     getAllDivision,
+     getSingleDivision,
+     updateDivision,
+     deleteDivision
+}
+ 
---
+++ src/app/modules/tour/tour.route.ts
@@ -0,0 +1,24 @@
+import { validateRequest } from './../../middlewares/validateRequest';
+import { Role } from '../user/user.interface';
+import { checkAuth } from './../../middlewares/checkAuth';
+import { Router } from "express";
+import { createTourTypeZodSchema, updateTourZodSchema } from './tour.validation';
+import { TourController } from './tour.controller';
+
+
+
+const router = Router();
+
+/* Tour Type Routes */
+router.post("/create-tour-type", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(createTourTypeZodSchema), TourController.createTourType)
+router.get("/tour-types", TourController.getAllTourTypes)
+router.patch("/tour-types/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(createTourTypeZodSchema), TourController.updateTourType)
+router.delete("/tour-types/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), TourController.deleteTourType)
+
+/* Tour Routes */
+router.post("/create", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(createTourTypeZodSchema), TourController.createTour)
+router.get("/", TourController.getAllTours)
+router.patch("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN),validateRequest(updateTourZodSchema), TourController.updateTour)
+router.delete("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), TourController.deleteTour)
+
+export const TourRoutes = router;
