7af9fa8d2cd7d6c340881d8faffc5f939e669567
Ridoy5315 <ridoy.st99@gmail.com>
2025-07-25 23:55:49 +0800

    redis configeration has been completed

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
+import { connectRedis } from "./app/config/redis.consfig";
 
 let server: Server;
 
@@ -23,6 +24,7 @@ const startServer = async () => {
 };
 
 (async () => {
+  await connectRedis()
   await startServer();
   await seedSuperAdmin();
 })();
